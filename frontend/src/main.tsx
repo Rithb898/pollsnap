2b73f1f09fe030db6c096dc2c175b24cbd23bbdb
Rithb898 <rithb2004@gmail.com>
2026-05-10 18:42:21 +0530

    feat: add frontend UI components and hooks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.tsx'
+import { TooltipProvider } from './components/ui/tooltip.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <TooltipProvider>
+          <Route path="/" element={<App />} />
+        </TooltipProvider>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
